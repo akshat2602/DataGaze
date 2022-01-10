60b0200c2e3b92c190ca13e48d7b8ddcdc80f341
Akshat Sharma <akshatsharma2602@gmail.com>
2022-01-10 14:26:27 +0530

    Added error handling for when database record already exists

--- server/connectionAPI/views.py
+++ server/connectionAPI/views.py
@@ -10,11 +10,27 @@ from connectionAPI.models import Database, Field, Table
 from .serializers import DatabaseSerializer
 # Create your views here.
 
+def check_already_existing_database(name, host, user):
+    try:
+        return Database.objects.get(name=name,
+                                host=host,
+                                username=user)
+    except Database.DoesNotExist:
+        return False
+
 class ConnectionViewSet(viewsets.ViewSet):
 
     @swagger_auto_schema(request_body=DatabaseSerializer, responses={200: DatabaseSerializer})
     @action(detail=False, methods=["post"], url_path="create")
     def connect_db(self, request, *args, **kwargs):
+        database_exists = check_already_existing_database(request.data["name"],
+                                                        request.data["host"],
+                                                        request.data["username"])
+
+        if isinstance(database_exists, Database):
+            db_serialized = DatabaseSerializer(instance=database_exists)
+            return Response(db_serialized.data, status=status.HTTP_200_OK)
+
         db_serialized = DatabaseSerializer(data=request.data)
         if db_serialized.is_valid():
             try:
@@ -23,9 +39,11 @@ class ConnectionViewSet(viewsets.ViewSet):
                                         port=db_serialized.validated_data["port"],
                                         user=db_serialized.validated_data["username"],
                                         password=db_serialized.validated_data["password"])
+
             except psycopg2.Error as e:
                 print(e)
                 return Response(status=status.HTTP_400_BAD_REQUEST)
+
             try:
                 cur = conn.cursor()
                 cur.execute("SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';")
@@ -51,12 +69,13 @@ class ConnectionViewSet(viewsets.ViewSet):
                     final_fields = [[field[0], field[1]] for field in fields]
                     print(final_fields)
                     Field.objects.bulk_create([
-                        Field(name = str(field[0]), data_type=str(field[1]), fk_table=ob) for field in final_fields
+                        Field(name = str(field[0]), 
+                        data_type=str(field[1]), 
+                        fk_table=ob) for field in final_fields
                     ])
                     
             except:
                 return Response(status=status.HTTP_400_BAD_REQUEST)
-
 
             cur.close()
             conn.close()
